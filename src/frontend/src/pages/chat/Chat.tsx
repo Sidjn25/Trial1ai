fe88006328c462ca4e843eea0fb081ef94800582
Sidjn25 <sidjn1997@gmail.com>
2025-05-13 14:30:12 +0530

    Update Chat.tsx

--- src/frontend/src/pages/chat/Chat.tsx
+++ src/frontend/src/pages/chat/Chat.tsx
@@ -201,8 +201,8 @@ const Chat = () => {
                     {!lastQuestionRef.current ? (
                         <div className={styles.chatEmptyState}>
                             <SparkleFilled fontSize={"120px"} primaryFill={"rgba(115, 118, 225, 1)"} aria-hidden="true" aria-label="Chat logo" />
-                            <h1 className={styles.chatEmptyStateTitle}>Product chat</h1>
-                            <h2 className={styles.chatEmptyStateSubtitle}>Ask anything or try an example</h2>
+                            <h1 className={styles.chatEmptyStateTitle}>Chat with HCDS</h1>
+                            <h2 className={styles.chatEmptyStateSubtitle}>Ask anything about HCDS</h2>
                             <ExampleList onExampleClicked={onExampleClicked} />
                         </div>
                     ) : (
